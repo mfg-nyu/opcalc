e28d5b1dcf4ddf1de0033c5d863edb198c8e65af
vic-lee <viclee@nyu.edu>
2020-12-27 03:10:43 -0500

    Add minimal opcalc usage example

--- example/opcalc-example-react/src/App.tsx
+++ example/opcalc-example-react/src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useMemo, useState } from "react";
 import "./App.css";
 
 enum LoadStatus {
@@ -7,6 +6,10 @@ enum LoadStatus {
   Loaded,
   LoadErred,
 }
+
+const getSecondTimestamp = (dt: Date): number => {
+  return Math.floor(dt.getTime() / 1000);
+};
 
 /**
  * Imports a wasm module, or signals error if an error has occured during import.
@@ -27,8 +30,8 @@ const useWasm = function <T>(loadPath: string) {
   });
 
   useEffect(() => {
-    import(loadPath)
-      .then((instance) => {
+    import("opcalc")
+      .then((instance: any) => {
         setState({ instance, loadStatus: LoadStatus.Loaded, error: undefined });
       })
       .catch((e) => {
@@ -49,23 +52,102 @@ const useWasm = function <T>(loadPath: string) {
   };
 };
 
+interface OptionDefinition {
+  assetPrice: number;
+  strike: number;
+  volatility: number;
+  interest: number;
+  currTime: Date;
+  expiryTime: Date;
+}
+
+const useOpCalc = (initialOptionDef: OptionDefinition) => {
+  type OpCalcType = typeof import("opcalc");
+  const { instance: opcalc } = useWasm<OpCalcType>("opcalc");
+
+  const [optionDef, setOptionDef] = useState<OptionDefinition>(
+    initialOptionDef
+  );
+
+  const outputs = useMemo(() => {
+    if (!opcalc) return {};
+    const {
+      assetPrice,
+      strike,
+      volatility,
+      interest,
+      currTime,
+      expiryTime,
+    } = optionDef;
+
+    console.log(optionDef);
+
+    try {
+      // const option = opcalc.WasmBSOptionBuilder.new()
+      //   .with_asset_price(assetPrice)
+      //   .with_strike(strike)
+      //   .with_volatility(volatility)
+      //   .with_interest(interest)
+      //   .with_current_time(getSecondTimestamp(currTime))
+      //   .with_maturity_time(getSecondTimestamp(expiryTime))
+      //   .create();
+
+      const option = opcalc.BSOption.new(
+        getSecondTimestamp(currTime),
+        getSecondTimestamp(expiryTime),
+        assetPrice,
+        strike,
+        interest,
+        volatility,
+        0
+      );
+
+      return {
+        call: {
+          value: option.call_value(),
+        },
+        put: {
+          value: option.put_value(),
+        },
+      };
+    } catch (error) {
+      console.error(error);
+      return {};
+    }
+  }, [opcalc, optionDef]);
+
+  return {
+    setOptionDef,
+    outputs,
+  };
+};
+
+const optionDef: OptionDefinition = {
+  assetPrice: 100,
+  strike: 105,
+  volatility: 0.23,
+  interest: 0.005,
+  currTime: new Date(2020, 11, 1),
+  expiryTime: new Date(2021, 0, 15),
+};
+
 function App() {
+  const { outputs } = useOpCalc(optionDef);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div>
+          <div className="option-entry">
+            <span className="label">Call value</span>
+            <span className="content">{outputs.call?.value}</span>
+          </div>
+          <div className="option-entry">
+            <span className="label">Put value</span>
+            <span className="content">{outputs.put?.value}</span>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
