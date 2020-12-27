169cf32ed016c725563e33232c13b17b5672bc1e
vic-lee <viclee@nyu.edu>
2020-12-26 23:28:14 -0500

    Add hook for using wasm module

--- example/opcalc-example-react/src/App.tsx
+++ example/opcalc-example-react/src/App.tsx
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
+enum LoadStatus {
+  NotLoaded,
+  Loaded,
+  LoadErred,
+}
+
+/**
+ * Imports a wasm module, or signals error if an error has occured during import.
+ *
+ * @typeParam T the type of module to be imported.
+ * @param loadPath Specify the path where wasm module should be imported from
+ *   (e.g. 'opcalc').
+ */
+const useWasm = function <T>(loadPath: string) {
+  const [state, setState] = useState<{
+    instance: T | undefined;
+    loadStatus: LoadStatus;
+    error: Error | undefined;
+  }>({
+    instance: undefined,
+    loadStatus: LoadStatus.NotLoaded,
+    error: undefined,
+  });
+
+  useEffect(() => {
+    import(loadPath)
+      .then((instance) => {
+        setState({ instance, loadStatus: LoadStatus.Loaded, error: undefined });
+      })
+      .catch((e) => {
+        console.error(e);
+        setState((prevState) => ({
+          ...prevState,
+          error: e,
+          loadStatus: LoadStatus.LoadErred,
+        }));
+      });
+  }, [loadPath]);
+
+  return {
+    instance: state.instance,
+    error: state.error,
+    loaded: state.loadStatus === LoadStatus.Loaded,
+    loadErred: state.loadStatus === LoadStatus.LoadErred,
+  };
+};
 
 function App() {
   return (
